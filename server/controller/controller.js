d15d7acba59fb7e773974a2951e6949d7557258c
Amares03 <amaressabit0314@gmail.com>
2022-04-19 12:25:43 +0300

    generate pdf

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,109 +1,112 @@
-var userdb = require('../model/model');
+var userdb = require("../model/model");
 var path = require("path");
-const pdfService = require('../services/pdf-service');
+const pdfService = require("../services/pdf-service");
 
 var options = {
-    base: "http://localhost:8080", // or use: req.protocol + '://' + req.get('host')
-    format: "A4",
-    orientation: "portrait",
-    border: "5mm",
-    
-    
+  base: "http://localhost:8080", // or use: req.protocol + '://' + req.get('host')
+  format: "A4",
+  orientation: "portrait",
+  border: "5mm",
 };
 
 let pdfDocument = {
-    html: '',
-    data: {},
-    type: "buffer", // "stream" || "buffer" || "" ("" defaults to pdf)
+  html: "",
+  data: {},
+  type: "buffer", // "stream" || "buffer" || "" ("" defaults to pdf)
 };
 
-exports.create = (req,res)=>{
-    if(!req.body){
-        res.status(400).send({message: 'user cannot be emity'});
-    }
-    const user = new userdb({
-        fullName:req.body.fullName,
-        passportNum:req.body.passportNum,
-        requestedDate:req.body.requestedDate,
-        collectedDate:req.body.collectedDate,
-        dbo:req.body.dbo,
-        nationality:req.body.nationality,
-        phone:req.body.phone,
-        resultDate:req.body.resultDate,
-        reviewedBy:req.body.reviewedBy,
-        sampleId:req.body.sampleId,
-        sex:req.body.sex,
+exports.create = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const user = new userdb({
+    fullName: req.body.fullName,
+    passportNum: req.body.passportNum,
+    requestedDate: req.body.requestedDate,
+    collectedDate: req.body.collectedDate,
+    dbo: req.body.dbo,
+    nationality: req.body.nationality,
+    phone: req.body.phone,
+    resultDate: req.body.resultDate,
+    reviewedBy: req.body.reviewedBy,
+    sampleId: req.body.sampleId,
+    sex: req.body.sex,
+  });
+
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "error while saving" + err });
     });
+};
 
-    user.save(user)
-    .then(data =>{
-        res.send(data);
-    }).catch(err =>{
-        res.status(500).send({message:'error while saving'+err});
-    })
-
-}
-
-exports.find = (req,res) =>{
-    if(!req.body){
-        res.status(400).send({message: 'user cannot be emity'});
-    }
-    const id = req.params.id;
-    userdb
+exports.find = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const id = req.params.id;
+  userdb
     .findById(id)
-    .then(data =>{
-        res.render('index',{user:data});
-    }).catch(err =>{
-        res.status(500).render('errorpage'+err);
+    .then((data) => {
+      res.render("index", { user: data });
     })
-} 
+    .catch((err) => {
+      res.status(500).render("errorpage" + err);
+    });
+};
 
-exports.detail = (req,res) =>{
-    if(!req.body){
-        res.status(400).send({message: 'user cannot be emity'});
-    }
-    const id = req.params.id;
-    userdb
+exports.detail = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const id = req.params.id;
+  userdb
     .findById(id)
-    .then(data =>{
-        res.render('detail',{user:data});
-        
-    }).catch(err =>{
-        res.status(500).render('errorpage'+err);
+    .then((data) => {
+      res.render("detail", { user: data });
     })
-} 
+    .catch((err) => {
+      res.status(500).render("errorpage" + err);
+    });
+};
 
-exports.findSample = (req,res) =>{
-    if(!req.body){
-        res.status(400).send({message: 'user cannot be emity'});
-    }
-    const id = req.params.id;
-    userdb
-    .find({sampleId:id})
-    .then(data =>{
-        res.render('index',{user:data[0]});
-    }).catch(err =>{
-        res.status(500).render('errorpage'+err);
+exports.findSample = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const id = req.params.id;
+  userdb
+    .find({ sampleId: id })
+    .then((data) => {
+      res.render("index", { user: data[0] });
     })
-} 
+    .catch((err) => {
+      res.status(500).render("errorpage" + err);
+    });
+};
 
-
-exports.pdfGenerate =  (req,res,next)=>{
-    if(!req.body){
-        res.status(400).send({message: 'user cannot be emity'});
-    }
-    const id = req.params.id;
-    userdb
+exports.pdfGenerate = (req, res, next) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const id = req.params.id;
+  userdb
     .findById(id)
-    .then(data =>{
-         res.writeHead(200,{
-            'Content-Type':'application/pdf',
-            'Content-Disposition':'attachment;filename=invoice.pdf'
-        });
-        pdfService.buildPDF(
-            data );
-    }).catch(err =>{
-        res.status(500).render('errorpage');
+    .then((data) => {
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Disposition", "attachment;filename=invoice.pdf");
+      //   res.writeHead(200, {
+      //     "Content-Type": "application/pdf",
+      //     "Content-Disposition": "attachment;filename=invoice.pdf",
+      //   });
+      console.log("gettted here");
+      pdfService.buildPDF(data);
+      console.log("pdf generated");
     })
-}
-
+    .catch((err) => {
+      res.status(500).render("errorpage");
+    });
+};
--- server/services/pdf-service.js
+++ server/services/pdf-service.js
@@ -1,223 +1,219 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 
-
 function buldPdf(data) {
-  
+  const pdf = new PDFDocument();
 
+  pdf.pipe(fs.createWriteStream("output.pdf"));
 
-const pdf = new PDFDocument();
+  const fullWidth = 595.28;
+  const fullHeight = 841.89;
 
-// pdf.pipe(fs.createWriteStream("output.pdf"));
+  // pdf header content
+  pdf.image("../../assets/img/header-img.png", 0, 0, {
+    height: 80,
+    width: fullWidth,
+  });
 
-const fullWidth = 595.28;
-const fullHeight = 841.89;
+  pdf.fontSize(16).text("Laboratory eReport", 0, 150, {
+    align: "center",
+    width: fullWidth,
+  });
+  pdf
+    .image("../../assets/img/men1.jpg", 10, 180, {
+      height: 130,
+    })
+    .fontSize(8)
+    .text("*Photo not taken during sample collection", 20, 300);
+  pdf
+    .image("../../assets/img/rq_link.jpg", 460, 180, {
+      width: 130,
+    })
+    .fontSize(8)
+    .text("*Scan the code to verify result", 470, 300, {
+      width: 120,
+    });
 
-// pdf header content
-pdf.image("../../assets/img/header-img.png", 0, 0, {
-  height: 80,
-  width: fullWidth,
-});
+  // Table -1 ***************************************************
+  pdf.moveTo(380, 330).lineTo(380, 390).lineWidth(0);
+  pdf.rect(20, 310, 555, 80).lineWidth(0).stroke();
+  pdf.rect(20, 310, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
 
-pdf.fontSize(16).text("Laboratory eReport", 0, 150, {
-  align: "center",
-  width: fullWidth,
-});
-pdf
-  .image("../../assets/img/men1.jpg", 10, 180, {
-    height: 130,
-  })
-  .fontSize(8)
-  .text("*Photo not taken during sample collection", 20, 300);
-pdf
-  .image("../../assets/img/rq_link.jpg", 460, 180, {
-    width: 130,
-  })
-  .fontSize(8)
-  .text("*Scan the code to verify result", 470, 300, {
+  // ***************************************************
+  pdf
+    .font("Times-Bold")
+    .fontSize(12)
+    .fillColor("#fff")
+    .text("Client Information", 25, 315);
+  pdf.font("Times-Roman").fontSize(10).fillColor("#000").text("Name", 25, 335);
+  pdf.fontSize(10).fillColor("#000").text("Passport", 25, 355);
+  pdf.fontSize(10).fillColor("#000").text("Nationality", 25, 375);
+
+  // ***************************************************
+  pdf.font("Times-Bold").fontSize(12).text(data.fullName, 150, 335, {
+    width: 120,
+  });
+  pdf.fontSize(12).text(data.passportNum, 150, 355, {
+    width: 120,
+  });
+  pdf.fontSize(12).text(data.nationality, 150, 375, {
+    width: 120,
+  });
+  // ***************************************************
+
+  pdf.font("Times-Roman").fontSize(10).fillColor("#000").text("DOB", 385, 335);
+  pdf.fontSize(10).fillColor("#000").text("Sex", 385, 355);
+  pdf.fontSize(10).fillColor("#000").text("Phone", 385, 375);
+
+  // ***************************************************
+  pdf.font("Times-Bold").fontSize(12).text(data.dbo, 450, 335, {
+    width: 120,
+  });
+  pdf.fontSize(12).text(data.sex, 450, 355, {
+    width: 120,
+  });
+  pdf.fontSize(12).text(data.phone, 450, 375, {
     width: 120,
   });
 
-// Table -1 ***************************************************
-pdf.moveTo(380, 330).lineTo(380, 390).lineWidth(0);
-pdf.rect(20, 310, 555, 80).lineWidth(0).stroke();
-pdf.rect(20, 310, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
+  // Table -2 ***************************************************
 
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fontSize(12)
-  .fillColor("#fff")
-  .text("Client Information", 25, 315);
-pdf.font("Times-Roman").fontSize(10).fillColor("#000").text("Name", 25, 335);
-pdf.fontSize(10).fillColor("#000").text("Passport", 25, 355);
-pdf.fontSize(10).fillColor("#000").text("Nationality", 25, 375);
+  pdf
+    .moveTo(380, 430)
+    .lineTo(380, 470)
+    .strokeColor("#000")
+    .lineWidth(0)
+    .stroke();
+  pdf.rect(20, 410, 555, 60).lineWidth(0).stroke();
+  pdf.rect(20, 410, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
 
-// ***************************************************
-pdf.font("Times-Bold").fontSize(12).text(data.fullName, 150, 335, {
-  width: 120,
-});
-pdf.fontSize(12).text(data.passportNum, 150, 355, {
-  width: 120,
-});
-pdf.fontSize(12).text(data.nationality, 150, 375, {
-  width: 120,
-});
-// ***************************************************
+  // *********************************************************
+  pdf
+    .font("Times-Bold")
+    .fontSize(12)
+    .fillColor("#fff")
+    .text("Specimen Information", 25, 415);
 
-pdf.font("Times-Roman").fontSize(10).fillColor("#000").text("DOB", 385, 335);
-pdf.fontSize(10).fillColor("#000").text("Sex", 385, 355);
-pdf.fontSize(10).fillColor("#000").text("Phone", 385, 375);
-
-// ***************************************************
-pdf.font("Times-Bold").fontSize(12).text(data.dbo, 450, 335, {
-  width: 120,
-});
-pdf.fontSize(12).text(data.sex, 450, 355, {
-  width: 120,
-});
-pdf.fontSize(12).text(data.phone, 450, 375, {
-  width: 120,
-});
-
-// Table -2 ***************************************************
-
-pdf.moveTo(380, 430).lineTo(380, 470).strokeColor("#000").lineWidth(0).stroke();
-pdf.rect(20, 410, 555, 60).lineWidth(0).stroke();
-pdf.rect(20, 410, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
-
-// *********************************************************
-pdf
-  .font("Times-Bold")
-  .fontSize(12)
-  .fillColor("#fff")
-  .text("Specimen Information", 25, 415);
-
-pdf
-  .font("Times-Roman")
-  .fontSize(10)
-  .fillColor("#000")
-  .text("Specimen Type", 25, 435);
-pdf.fontSize(10).fillColor("#000").text("Collected location", 25, 455);
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fillColor("#000")
-  .fontSize(12)
-  .text(data.specimenType, 150, 435, {
+  pdf
+    .font("Times-Roman")
+    .fontSize(10)
+    .fillColor("#000")
+    .text("Specimen Type", 25, 435);
+  pdf.fontSize(10).fillColor("#000").text("Collected location", 25, 455);
+  // ***************************************************
+  pdf
+    .font("Times-Bold")
+    .fillColor("#000")
+    .fontSize(12)
+    .text(data.specimenType, 150, 435, {
+      width: 120,
+    });
+  pdf.fontSize(12).text(data.collectedLocation, 150, 455, {
     width: 120,
   });
-pdf.fontSize(12).text(data.collectedLocation, 150, 455, {
-  width: 120,
-});
 
-// ***************************************************
+  // ***************************************************
 
-pdf
-  .font("Times-Roman")
-  .fontSize(10)
-  .fillColor("#000")
-  .text("Sample ID", 385, 435);
-pdf.fontSize(10).fillColor("#000").text("Collected", 385, 455);
+  pdf
+    .font("Times-Roman")
+    .fontSize(10)
+    .fillColor("#000")
+    .text("Sample ID", 385, 435);
+  pdf.fontSize(10).fillColor("#000").text("Collected", 385, 455);
 
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fillColor("#000")
-  .fontSize(12)
-  .text(data.sampleId, 450, 435, {
+  // ***************************************************
+  pdf
+    .font("Times-Bold")
+    .fillColor("#000")
+    .fontSize(12)
+    .text(data.sampleId, 450, 435, {
+      width: 120,
+    });
+  pdf.fontSize(12).text(data.collectedDate, 450, 455, {
     width: 120,
   });
-pdf.fontSize(12).text(data.collectedDate, 450, 455, {
-  width: 120,
-});
 
-// Table -3 *********************************************
-pdf.rect(20, 490, 555, 60).lineWidth(0).strokeColor("#000").stroke();
-pdf.rect(20, 490, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
-// *********************************************************
-pdf
-  .font("Times-Bold")
-  .fontSize(12)
-  .fillColor("#fff")
-  .text("Test Result", 25, 495);
+  // Table -3 *********************************************
+  pdf.rect(20, 490, 555, 60).lineWidth(0).strokeColor("#000").stroke();
+  pdf.rect(20, 490, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
+  // *********************************************************
+  pdf
+    .font("Times-Bold")
+    .fontSize(12)
+    .fillColor("#fff")
+    .text("Test Result", 25, 495);
 
-pdf
-  .font("Times-Roman")
-  .fontSize(10)
-  .fillColor("#000")
-  .text("SARS COV-2 (RT-PCR)", 25, 515);
-pdf.fontSize(10).fillColor("#000").text("Reported time (GMT+3)", 25, 535);
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fillColor("#000")
-  .fontSize(12)
-  .text(data.result, 200, 515, {
+  pdf
+    .font("Times-Roman")
+    .fontSize(10)
+    .fillColor("#000")
+    .text("SARS COV-2 (RT-PCR)", 25, 515);
+  pdf.fontSize(10).fillColor("#000").text("Reported time (GMT+3)", 25, 535);
+  // ***************************************************
+  pdf
+    .font("Times-Bold")
+    .fillColor("#000")
+    .fontSize(12)
+    .text(data.result, 200, 515, {
+      width: 120,
+    });
+  pdf.fontSize(12).text(data.resultDate, 200, 535, {
     width: 120,
   });
-pdf.fontSize(12).text(data.resultDate, 200, 535, {
-  width: 120,
-});
-// Table -4 ***************************************************
+  // Table -4 ***************************************************
 
-pdf.rect(20, 570, 555, 80).lineWidth(0).strokeColor("#000").stroke();
-pdf.rect(20, 570, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
+  pdf.rect(20, 570, 555, 80).lineWidth(0).strokeColor("#000").stroke();
+  pdf.rect(20, 570, 555, 20).lineWidth(0).fillAndStroke("#0054A7");
 
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fontSize(12)
-  .fillColor("#fff")
-  .text("Test Information", 25, 575);
-pdf
-  .font("Times-Roman")
-  .fontSize(10)
-  .fillColor("#000")
-  .text("Test Performed By", 25, 595);
-pdf.fontSize(10).fillColor("#000").text("Result Reviewed By", 25, 615);
-pdf.fontSize(10).fillColor("#000").text("Test Method", 25, 635);
+  // ***************************************************
+  pdf
+    .font("Times-Bold")
+    .fontSize(12)
+    .fillColor("#fff")
+    .text("Test Information", 25, 575);
+  pdf
+    .font("Times-Roman")
+    .fontSize(10)
+    .fillColor("#000")
+    .text("Test Performed By", 25, 595);
+  pdf.fontSize(10).fillColor("#000").text("Result Reviewed By", 25, 615);
+  pdf.fontSize(10).fillColor("#000").text("Test Method", 25, 635);
 
-// ***************************************************
-pdf
-  .font("Times-Bold")
-  .fontSize(12)
-  .text(data.testPerformedBy, 200, 595, {
+  // ***************************************************
+  pdf.font("Times-Bold").fontSize(12).text(data.testPerformedBy, 200, 595, {
     width: 200,
   });
-pdf.fontSize(12).text(data.reviewedBy, 200, 615, {
-  width: 200,
-});
-pdf
-  .fontSize(12)
-  .text(data.testMethod, 200, 635, {
+  pdf.fontSize(12).text(data.reviewedBy, 200, 615, {
+    width: 200,
+  });
+  pdf.fontSize(12).text(data.testMethod, 200, 635, {
     width: 400,
   });
-// ***************************************************
+  // ***************************************************
 
-// Footer ********************************************
-pdf.image("../../assets/img/footer.png", 10, 720, {
-  height: 100,
-  width: fullWidth - 25,
-});
-pdf
-  .moveTo(0, fullHeight - 10)
-  .lineTo(440, fullHeight - 10)
-  .strokeColor("#0054A7")
-  .lineWidth(10)
-  .stroke();
-pdf
-  .font("Times-Italic")
-  .fontSize(12)
-  .fillColor("#0054A7")
-  .text("Turning Science to Care", 450, fullHeight - 15, {
-    width: 150,
-    height: 0,
+  // Footer ********************************************
+  pdf.image("../../assets/img/footer.png", 10, 720, {
+    height: 100,
+    width: fullWidth - 25,
   });
+  pdf
+    .moveTo(0, fullHeight - 10)
+    .lineTo(440, fullHeight - 10)
+    .strokeColor("#0054A7")
+    .lineWidth(10)
+    .stroke();
+  pdf
+    .font("Times-Italic")
+    .fontSize(12)
+    .fillColor("#0054A7")
+    .text("Turning Science to Care", 450, fullHeight - 15, {
+      width: 150,
+      height: 0,
+    });
 
-  doc.on('data', dataCallback);
-  doc.on('end', endCallback);
-pdf.end();
+  // doc.on("data", dataCallback);
+  // doc.on("end", endCallback);
+  pdf.end();
 }
 
-module.exports = {buldPdf};
+module.exports = { buldPdf };
